c5165b92c8b904ea226cd21e6891de454c7c17fd
Manuela Flores <fmanuela499@gmail.com>
2020-05-12 23:08:31 -0500

    feat(main): add some booleans expressions :art: :art:

--- app/src/main/java/com/manuflowers/trainingkotlin/Main.kt
+++ app/src/main/java/com/manuflowers/trainingkotlin/Main.kt
@@ -36,4 +36,26 @@ fun main(arguments: Array<String>) {
 
     val nameLength = fullName.length
     println(nameLength)
+
+    //Booleans
+    val isProgrammingAwesome = true
+    println("Is programming awesome? $isProgrammingAwesome")
+
+    val isLegalDrivingAge = myAge >= 18
+    println(isLegalDrivingAge)
+
+    val ageTim = 27
+    val isTimOlder = ageTim > myAge
+    println(isTimOlder)
+
+    val yearOfBirth = 1996
+    val isBornInTwentiethCentury = yearOfBirth < 2000
+    println(isBornInTwentiethCentury)
+
+    val strangerName = "Tim"
+    println(myName == strangerName)
+
+    val areNamesDifferent = myName != strangerName
+    println(areNamesDifferent)
+
 }
