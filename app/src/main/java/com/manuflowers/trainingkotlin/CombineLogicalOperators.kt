6b08a16b9796ad5f9ce537209792beb6a7ae1bfb
Manuela Flores <fmanuela499@gmail.com>
2020-05-14 11:49:36 -0500

    feat(main): add logical operators example :fire: :fire:

--- app/src/main/java/com/manuflowers/trainingkotlin/CombineLogicalOperators.kt
+++ app/src/main/java/com/manuflowers/trainingkotlin/CombineLogicalOperators.kt
@@ -1,5 +1,16 @@
 package com.manuflowers.trainingkotlin
 
 fun main() {
+    val myAge = 12
+    val country = "USA"
+    val isLegalDriving = (myAge >= 18 && country == "Perú") || (myAge >= 16 && country == "USA")
+    println(isLegalDriving)
 
+    if (isLegalDriving) {
+        println("You can drive :D!")
+    } else {
+        println("You've got a bit time to go :(")
+    }
+
+    println( if (myAge >= 18) println("I'm an adult") else println("I'm still underaged!"))
 }
