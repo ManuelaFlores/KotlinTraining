4f70cf6f054f841348e0370914f5261c3627fe3f
Manuela Flores <fmanuela499@gmail.com>
2020-05-15 00:16:57 -0500

    wip :recycle:

--- app/src/main/java/com/manuflowers/trainingkotlin/CreateAndConsumeNullables.kt
+++ app/src/main/java/com/manuflowers/trainingkotlin/CreateAndConsumeNullables.kt
@@ -3,7 +3,7 @@ package com.manuflowers.trainingkotlin
 fun main() {
     val myName = "Manuela"
     val nickName: String? = "Manu"
-    val lastName: String?= null
+    val lastName: String? = null
 
     println(nickName)
 
@@ -15,5 +15,23 @@ fun main() {
     } else {
         println("I don't have a last name")
     }
+
+    if (nickName?.isEmpty() == true) {
+        println("You don't have a nickname! It's length is ${nickName.length}")
+    }
+
+    /*val notNullableNickname = requireNotNull(nickName)
+    println(notNullableNickname.length)*/
+
+    val myNickname = nickName ?: myName
+    println(myNickname)
+
+    val lastBirthdayYear = 2020
+    val myAge: Int? = 24
+    val myAgeNotNull = myAge ?: return
+
+    val yearOfBirth = lastBirthdayYear - myAgeNotNull
+    println("I was born in $yearOfBirth")
+
 
 }
