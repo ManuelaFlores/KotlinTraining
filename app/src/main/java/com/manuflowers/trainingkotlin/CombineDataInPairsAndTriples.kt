ff2b478f6a665c9b5566ed5c2608f51799a24e22
Manuela Flores <fmanuela499@gmail.com>
2020-05-19 01:18:12 -0500

    add first part of pairs and triples :sparkles:

---
+++ app/src/main/java/com/manuflowers/trainingkotlin/CombineDataInPairsAndTriples.kt
@@ -0,0 +1,19 @@
+package com.manuflowers.trainingkotlin
+
+fun main() {
+    /**
+     * In Kotlin there are four types to store values in collections and there are
+     *  - Lists
+     *  - Arrays
+     *  - Maps
+     *  - Sets
+     *  Lists and arrays  and values can store multiple elements, you can have duplicates here.
+     *  Sets are use to store data which is unique and can not have duplicates.
+     *  Maps are use to define key-value pairs just like words in a dictionary.
+     * */
+    val fullName = Pair<String, String>("Manuela", "Flores")
+    println("My name is ${fullName.first} and my last name is ${fullName.second}")
+
+    //This way of calling values is destructuring:
+
+}
