77239e0d536368ac071ca8c7a646b9cbd041e376
Manuela Flores <fmanuela499@gmail.com>
2020-05-27 01:03:06 -0500

    wip :recycle:

--- app/src/main/java/com/manuflowers/trainingkotlin/CreateAndAccessMap.kt
+++ app/src/main/java/com/manuflowers/trainingkotlin/CreateAndAccessMap.kt
@@ -20,4 +20,45 @@ fun main() {
         "content/type" to "application/json"
     )
     println(authenticationHeaders)
+
+    //-----
+
+    moviesCollection["Comedy"] = mutableListOf("Pitch Perfects")
+    println(moviesCollection)
+
+    moviesCollection["Comedy"]?.add("Going in Style")
+    println(moviesCollection)
+
+    val removedActionGames = moviesCollection.remove("Action")
+    println(removedActionGames)
+    println(moviesCollection)
+
+    /**
+     * There are different layer of mutability and immutability :
+     *  - Referential mutability:
+     *  You can or can not change the reference of a value: val name = "Manu" can not change its value
+     *  But just because something is a val it doesn't means that you can not change it, it means you can not change its reference
+     *  - Internal mutability
+     *  If you have a list which is an array of strings: listOf("Tim", "Sam", "Ana")
+     *  you can change each of the strings internally, in that way you're not changing the structure(the array still have the same size and reference) :)
+     *  - Content mutability:
+     *  If you have a val which is a map so it can not change its references or its internal structure but you can still access a mutable value with in
+     *  and change that value structure
+     * */
+
+    moviesCollection["Comedy"]?.add("Central Intelligence")
+    moviesCollection["Romance"] = mutableListOf("Bridesmaid")
+
+    println("-----")
+    for(key in moviesCollection) {
+        println(key)
+    }
+
+    for (value in moviesCollection.values) {
+        println(value)
+    }
+
+    for ((key, value) in moviesCollection){
+        println("This is the key $key and this is the value: $value")
+    }
 }
