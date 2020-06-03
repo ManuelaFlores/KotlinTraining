dbdd0a636b374245b965dc12f6437dfc1587d049
Manuela Flores <fmanuela499@gmail.com>
2020-06-02 22:55:07 -0500

    add more about delegated and non-delegated properties :art: :bulb: :sparkles:

--- app/src/main/java/com/manuflowers/trainingkotlin/DelegatedProperties.kt
+++ app/src/main/java/com/manuflowers/trainingkotlin/DelegatedProperties.kt
@@ -1,5 +1,6 @@
 package com.manuflowers.trainingkotlin
 
+import kotlin.math.atan
 import kotlin.properties.Delegates
 
 /**
@@ -12,8 +13,18 @@ import kotlin.properties.Delegates
  *
  * If you have a property that might take some time to calculate
  * and you don't want to slow things down until you actually need the property: Say hello to lazy properties
+ * Because the value only change once you can use val instead of var
+ * But, What if you just want to denote that a property will not have a value in the class instance is created;
+ * for that you have lateinit
  *
  * */
+
+class Circle(var radious: Double) {
+    val pi: Double = ((4.0 * atan(1.0 / 0.5)) - atan(1.0 / 239.0)) * 4.0
+    val circumference: Double
+        get() = pi * radious * 2
+
+}
 
 class LevelOfGame(val id: Int, val boss: String) {
 
@@ -27,6 +38,10 @@ class LevelOfGame(val id: Int, val boss: String) {
     companion object {
         var highestLevel = 1
     }
+}
+
+class Lamp {
+    lateinit var bulb: LightBulb
 }
 
 class LightBulb {
@@ -46,11 +61,18 @@ class LightBulb {
 
 fun main() {
 
+    val circle = Circle(5.0)
+    println(circle.circumference)
+
     val light = LightBulb()
     light.current = 90000
     println(light.current)
     light.current = 40
     println(light.current)
+
+    val fancyLamp = Lamp()
+    fancyLamp.bulb = light
+    println(fancyLamp.bulb.current)
 
     val level1 = LevelOfGame(id = 1, boss = "Outside Cat")
     val level2 = LevelOfGame(id = 2, boss = "Laser Point")
